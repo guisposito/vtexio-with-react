8c460cd53bd22ae9757a4e3e4f64150241186bc6
Guisposito <guicarlosjoinhas@gmail.com>
2024-03-15 18:03:18 -0300

    init repo

---
+++ react/components/h1component/index.js
@@ -0,0 +1,14 @@
+import React from 'react'
+import "../../styles/css/h1component.css"
+
+const h1component = () => {
+  const meuh1 = "Teste componente";
+
+  return (
+    <div className="meucomponente">
+      {meuh1}
+    </div>
+  )
+}
+
+export default h1component
---
+++ react/h1component.js
@@ -0,0 +1,3 @@
+import h1component from "./components/h1component";
+
+export default h1component
